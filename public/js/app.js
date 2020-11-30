0f953a6989e35fd82d19959b77dcac1b33452b0e
unknown <nsabelita0144@gmail.com>
2020-11-30 14:48:29 +0800

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,9 +8,10 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Loading... Please wait.'
     messageTwo.textContent = ''
+
     const location = search.value
     
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
